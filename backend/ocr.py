7327b575a866a84e070b77420cece0027210062d
DyuthiVivek <dyuthi.vivek@gmail.com>
2025-03-26 18:24:40 +0530

    restore ocr

--- backend/ocr.py
+++ backend/ocr.py
@@ -1,9 +1,9 @@
 import easyocr
 from PIL import Image
 from pdfminer.high_level import extract_text
-# from google import genai
-import google.generativeai as genai
-
+from google import genai
+# import google.generativeai as genai
+from pdf2image import convert_from_path
 from pydantic import BaseModel
 from typing import Optional
 from dotenv import load_dotenv
