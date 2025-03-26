1b8ae6273a74d3c239b2667435af251248015d4b
DyuthiVivek <dyuthi.vivek@gmail.com>
2025-03-26 19:25:07 +0530

    AI insights for reports

--- backend/ai_insights.py
+++ backend/ai_insights.py
@@ -1,4 +1,4 @@
-import google.generativeai as genai
+from google import genai
 from dotenv import load_dotenv
 import os
 import asyncio
@@ -10,27 +10,16 @@ load_dotenv()
 api_key = os.getenv("GENAI_API_KEY")
 
 
-async def get_insights_async(filepath):
-    input_text = extract_text_from_file(filepath, isText=False)
-    input_text = 'Extract the primary diagnosis, blood group, comorbidity, allergies from the following text: ' + input_text
+async def get_insights_async(text):
+    input_text = "This is a differently abled student's progress report. Generate insights on the student and their progress, and generate insights on what the educator can do to help the student. Output only the insights, do not give any other text." + text
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model='gemini-2.0-flash',
         contents=input_text,
-        config={
-            'response_mime_type': 'application/json',
-            'response_schema': MedicalReportInfo,
-        },
     )
     
-    medical_info: MedicalReportInfo = response.parsed
-    medical_info_dict = {
-        "primary_diagnosis": medical_info.primary_diagnosis,
-        "blood_group": medical_info.blood_group,
-        "comorbidity": medical_info.comorbidity,
-        "allergies": medical_info.allergies
-    }    
-    return medical_info_dict
+    return response.candidates[0].content.parts[0].text  # Accessing the text content
 
-def get_insights(filepath):
-    return asyncio.run(get_insights_async(filepath))
+
+def get_insights(text):
+    return asyncio.run(get_insights_async(text))
